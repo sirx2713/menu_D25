f79b12b27b253bc9c9fdb9f398824a47925081a9
sirx2713 <bradx2713@gmail.com>
2024-03-14 12:31:40 +0300

    Initial Commit

---
+++ menu_D25/menu_D25App.swift
@@ -0,0 +1,17 @@
+//
+//  menu_D25App.swift
+//  menu_D25
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/03/14.
+//
+
+import SwiftUI
+
+@main
+struct menu_D25App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
